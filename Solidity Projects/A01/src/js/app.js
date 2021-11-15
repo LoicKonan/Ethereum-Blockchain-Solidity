658544a055cc26eed572d22dd29c01c55a908e3f
Loic <konanloic10@gmail.com>
2021-11-15 01:33:01 -0600

    README.md

--- Solidity Projects/A01/src/js/app.js
+++ Solidity Projects/A01/src/js/app.js
@@ -7,17 +7,22 @@ App = {
   tokensSold: 0,
   tokensAvailable: 750000,
 
-  init: function() {
+  init: function() 
+  {
     console.log("App initialized...")
     return App.initWeb3();
   },
 
-  initWeb3: function() {
-    if (typeof web3 !== 'undefined') {
+  initWeb3: function() 
+  {
+    if (typeof web3 !== 'undefined') 
+    {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
-    } else {
+    } 
+    else 
+    {
       // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
@@ -25,18 +30,24 @@ App = {
     return App.initContracts();
   },
 
-  initContracts: function() {
-    $.getJSON("DappTokenSale.json", function(dappTokenSale) {
+  initContracts: function() 
+  {
+    $.getJSON("DappTokenSale.json", function(dappTokenSale) 
+    {
       App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
       App.contracts.DappTokenSale.setProvider(App.web3Provider);
-      App.contracts.DappTokenSale.deployed().then(function(dappTokenSale) {
+      App.contracts.DappTokenSale.deployed().then(function(dappTokenSale) 
+      {
         console.log("Dapp Token Sale Address:", dappTokenSale.address);
       });
-    }).done(function() {
-      $.getJSON("DappToken.json", function(dappToken) {
+    }).done(function() 
+    {
+      $.getJSON("DappToken.json", function(dappToken) 
+      {
         App.contracts.DappToken = TruffleContract(dappToken);
         App.contracts.DappToken.setProvider(App.web3Provider);
-        App.contracts.DappToken.deployed().then(function(dappToken) {
+        App.contracts.DappToken.deployed().then(function(dappToken) 
+        {
           console.log("Dapp Token Address:", dappToken.address);
         });
 
@@ -47,20 +58,26 @@ App = {
   },
 
   // Listen for events emitted from the contract
-  listenForEvents: function() {
-    App.contracts.DappTokenSale.deployed().then(function(instance) {
-      instance.Sell({}, {
+  listenForEvents: function() 
+  {
+    App.contracts.DappTokenSale.deployed().then(function(instance) 
+    {
+      instance.Sell({}, 
+        {
         fromBlock: 0,
         toBlock: 'latest',
-      }).watch(function(error, event) {
+      }).watch(function(error, event) 
+      {
         console.log("event triggered", event);
         App.render();
       })
     })
   },
 
-  render: function() {
-    if (App.loading) {
+  render: function() 
+  {
+    if (App.loading) 
+    {
       return;
     }
     App.loading = true;
@@ -72,8 +89,10 @@ App = {
     content.hide();
 
     // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      if(err === null) {
+    web3.eth.getCoinbase(function(err, account) 
+    {
+      if(err === null) 
+      {
         App.account = account;
         $('#accountAddress').html("Your Account: " + account);
       }
