b6a3d35b39fc10fa23efd53c605115823b1678ad
Loic <konanloic10@gmail.com>
2022-02-07 01:49:44 -0600

    README.md

--- Solidity Lessons/A01/main.cpp
+++ Solidity Lessons/A01/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 void countoff(int n)
 {
-  if(n == 0) cout << "Blast-off";
-  else cout << n;
-  countoff(n-1);
+  if(n == 0) 
+  cout << "Blast-off";
+  
+  else 
+  {
+    cout << n;
+    countoff(n-1);
+  }
 }
 
-main
+int main()
+{
+  countoff(2);
+
+  return 0;
+}
